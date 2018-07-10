8484e4bf6d5a998d1020c97d9f46151efd908dec
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-10 15:45:15 +0100

    file adding

--- src/main/java/models/File.java
+++ src/main/java/models/File.java
@@ -1,7 +1,13 @@
 package models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
 public class File {
 
+    private int id;
     private String name;
     private String extension;
     private int fileSize;
@@ -17,5 +23,28 @@ public class File {
         this.fileSize = fileSize;
         this.folder = folder;
     }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+
 
 }
