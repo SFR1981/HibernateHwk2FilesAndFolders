1f204497094b47db125bac505807a6c91392c890
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-10 15:55:29 +0100

    dbhelper set up

--- src/main/java/models/Folder.java
+++ src/main/java/models/Folder.java
@@ -1,9 +1,13 @@
 package models;
 
+import javax.persistence.*;
 import java.util.List;
 
-public class Folder {
 
+@Entity
+@Table(name = "folders")
+public class Folder {
+    private int id;
     private String title;
     private List<File> files;
 
@@ -15,4 +19,36 @@ public class Folder {
         this.title = title;
 
     }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId(){
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    @Column(name = "title")
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String newTitle){
+        this.title = newTitle;
+
+    }
+
+    @OneToMany(mappedBy = "folder", fetch = FetchType.LAZY)
+    public List<File> getFiles(){
+        return files;
+    }
+
+    public void setFiles(List<File> files){
+        this.files = files;
+    }
+
 }
